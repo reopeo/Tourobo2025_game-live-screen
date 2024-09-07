63af72f63573918dc519752def6844ec6850f1e5
Rin Iwai <rin@eyrin.jp>
2024-09-07 17:33:39 +0900

    Revert "port変更"
    This reverts commit 9797ba5e8294ebdf858fc635dbcc894604cea1a2.

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ let match: Match | null = null;
 let rosConnected = false;
 function rosConnect() {
   if (!rosConnected) {
-    const ros = new ROSLIB.Ros({ url: `ws://${location.hostname}:8766` });
+    const ros = new ROSLIB.Ros({ url: `ws://${location.hostname}:8765` });
 
     ros.on('connection', () => {
       console.log('connected');
